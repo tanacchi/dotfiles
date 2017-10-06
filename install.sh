fa8f23ea2e3e404576ca6c1de9a6b0af821b3a8a
tanacchi <q111026d@mail.kyutech.jp>
2017-10-06 10:43:31 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ln -sfn /home/tanacchi/dotfiles/.bashrc    /home/tanacchi/
-ln -sfn /home/tanacchi/dotfiles/.gitconfig /home/tanacchi/
-ln -rsfn /home/tanacchi/dotfiles/.emacs.d   /home/tanacchi/
-ln -sfn /home/tanacchi/dotfiles/.inputrc   /home/tanacchi/
+ln -sfn /home/tanacchi/dotfiles/.bashrc           /home/tanacchi/
+ln -sfn /home/tanacchi/dotfiles/.gitconfig        /home/tanacchi/
+ln -rsfn /home/tanacchi/dotfiles/.emacs.d/init.el /home/tanacchi/
+ln -sfn /home/tanacchi/dotfiles/.inputrc          /home/tanacchi/
