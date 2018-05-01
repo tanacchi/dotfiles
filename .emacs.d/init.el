3f343e8d06b568cfb3114b01939626590111b60c
tanacchi <q111026d@mail.kyutech.jp>
2018-05-01 15:20:50 +0900

    Add web-mode for emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -138,3 +138,20 @@
 ;; golden ratio
 (golden-ratio-mode 1)
 (add-to-list 'golden-ratio-exclude-buffer-names " *NeoTree*")
+
+;; For web-mode
+(setq web-mode-engines-alist
+      '(("php"    . "\\.phtml\\'")
+        ("blade"  . "\\.blade\\."))
+)
+
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
