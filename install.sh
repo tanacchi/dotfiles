4257c90237c456584b26f1780d4e6b3a044f2a8a
tanacchi <q111026d@mail.kyutech.jp>
2018-05-10 15:13:18 +0900

    Modify install.bash for git config

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-dotfiles=${PWD}
-
-ln -sfn  ${dotfiles}/.bashrc    ${HOME}/
-ln -sfn  ${dotfiles}/.gitconfig ${HOME}/
-ln -sfn  ${dotfiles}/.emacs.d   ${HOME}/
-ln -sfn  ${dotfiles}/.inputrc   ${HOME}/
