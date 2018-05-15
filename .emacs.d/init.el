3d63b5a6f6d99eb6706b4d0e15e5409cf660218b
tanacchi <q111026d@mail.kyutech.jp>
2018-05-15 19:00:46 +0900

    Add tab-width config for web-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -153,5 +153,6 @@
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+
+(setq web-mode-markup-indent-offset 2)
