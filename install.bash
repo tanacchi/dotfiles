2657503ed2b8df151763cedef40ce00d99645957
tanacchi <q111026d@mail.kyutech.jp>
2018-05-22 10:29:43 +0900

    Remove unnecessary back_slash

--- install.bash
+++ install.bash
@@ -7,7 +7,7 @@ ln -sfn  ${dotfiles}/.gitconfig ${HOME}/
 ln -sfn  ${dotfiles}/.emacs.d   ${HOME}/
 ln -sfn  ${dotfiles}/.inputrc   ${HOME}/
 
-read -p "Please set your \'user name\' on git" git_user_name
+read -p "Please set your 'user name' on git" git_user_name
 git config --global user.name ${git_user_name}
-read -p "Please set your \'email\' on git" git_email
+read -p "Please set your 'email' on git" git_email
 git config --global user.email ${git_email}
