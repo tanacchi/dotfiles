937d56fac3bf28ad06f08e251029955c30608d90
tanacchi <q111026d@mail.kyutech.jp>
2018-07-27 20:06:13 +0900

    Add configure for lua-mode to init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -168,5 +168,11 @@
 ;; el-get
 (add-to-list 'load-path (locate-user-emacs-file "el-get"))
 (require 'el-get)
-;; el-getでダウンロードしたパッケージは ~/.emacs.d/ に入るようにする
-(setq el-get-dir (locate-user-emacs-file ""))
+
+;;;; This snippet enables lua-mode
+;; This line is not necessary, if lua-mode.el is already on your load-path
+(add-to-list 'load-path "~/.emacs.d/lua-mode")
+
+(autoload 'lua-mode "lua-mode" "Lua editing mode." t)
+(add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
+(add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
