a5c30c4dd0393e6403ec52055a273d3f07886b14
tanacchi <q111026d@mail.kyutech.jp>
2018-11-23 16:58:34 +0900

    Remove unnecessary code on rails_installer.sh

--- rails_installer.sh
+++ rails_installer.sh
@@ -22,20 +22,16 @@ fi
 if [ ! `which rbenv` ]; then
   git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 
-  echo -e '\n\nexport PATH=$HOME/.rbenv/bin:$PATH' >> ~/.bashrc
-
   export PATH=$HOME/.rbenv/bin:$PATH
   source ~/.bashrc
   hash -r
 
-  echo 'eval "$(rbenv init -)"' >> ~/.bashrc
   eval "$(rbenv init -)"
 
   source ~/.bashrc
   hash -r
 
   git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
-  echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
 
   source ~/.bashrc
   hash -r
