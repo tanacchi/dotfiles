8dfbd28815a80084427d997653a433573fce8ec9
tanacchi <q111026d@mail.kyutech.jp>
2019-01-03 16:21:30 +0900

    Link vim configure files in installing

--- install.bash
+++ install.bash
@@ -6,11 +6,13 @@ read -p "Please set your 'user name' on git : " git_user_name
 read -p "Please set your 'email' on git : " git_email
 echo -e "[user]\n\tname = ${git_user_name}\n\temail = ${git_email}\n" > ${dotfiles}/.gitconfig.user
 
-ln -sfn  ${dotfiles}/.bashrc    ${HOME}/
-ln -sfn  ${dotfiles}/.gitconfig ${HOME}/
+ln -sfn  ${dotfiles}/.bashrc         ${HOME}/
+ln -sfn  ${dotfiles}/.gitconfig      ${HOME}/
 ln -sfn  ${dotfiles}/.gitconfig.user ${HOME}/
-ln -sfn  ${dotfiles}/.emacs.d   ${HOME}/
-ln -sfn  ${dotfiles}/.inputrc   ${HOME}/
+ln -sfn  ${dotfiles}/.emacs.d        ${HOME}/
+ln -sfn  ${dotfiles}/.inputrc        ${HOME}/
+ln -sfn  ${dotfiles}/.vimrc          ${HOME}/
+ln -sfn  ${dotfiles}/.vim            ${HOME}/
 
 terminator_config_dir="${HOME}/.config/terminator"
 if [ ! -d ${terminator_config_dir} ]; then
