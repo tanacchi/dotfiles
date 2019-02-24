1d68de26138360c75dddd8484c2810b64987c688
tanacchi <q111026d@mail.kyutech.jp>
2019-02-24 22:47:52 +0900

    Remove unnecessary emacs plugin

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -157,11 +157,3 @@
 ;; load-path で ~/.emacs.d とか書かなくてよくなる
 (when load-file-name
   (setq user-emacs-directory (file-name-directory load-file-name)))
-
-;;;; This snippet enables lua-mode
-;; This line is not necessary, if lua-mode.el is already on your load-path
-(add-to-list 'load-path "~/.emacs.d/lua-mode")
-
-(autoload 'lua-mode "lua-mode" "Lua editing mode." t)
-(add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
-(add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
