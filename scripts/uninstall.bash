bedd53f7c9565225db2325247fb09b3e281859d3
tanacchi <q111026d@mail.kyutech.jp>
2019-03-14 19:32:27 +0900

    Add uninstall script

---
+++ scripts/uninstall.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -eu
+
+dotfiles_dir="$(cd "$(dirname ${0})"; pwd)/../"
+
+declare -a dotfiles=(
+  ".bashrc" ".gitconfig" ".gitconfig.user" 
+  ".emacs.d" ".inputrc" ".vimrc" ".vim"
+)
+
+for target in ${dotfiles[@]}; do
+  if [ -e "${HOME}/${target}" ]; then
+    rm "${HOME}/${target}"
+  fi
+
+  if [ -e "${HOME}/${target}.backup" ]; then
+    mv "${HOME}/${target}.backup" "${HOME}/${target}"
+  fi
+done
+
+if [ ! -e "${HOME}/.bashrc" ]; then
+  cp "/etc/skel/.bashrc" "${HOME}"
+fi
