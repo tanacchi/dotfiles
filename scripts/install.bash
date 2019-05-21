96d244b6fed98e95ab5b63f221a878559d591d31
tanacchi <q111026d@mail.kyutech.jp>
2019-05-21 21:11:27 +0900

    Fix dot-files path on installer

--- scripts/install.bash
+++ scripts/install.bash
@@ -10,7 +10,7 @@ if [ ! -f ${dotfiles_dir}/.gitconfig.user ]; then
   create_git_user_config
 fi
 
-dotfiles=`ls -a  /home/tanacchi/dotfiles/  | tr ' ' '\n' | grep -E "^\..*$" | grep -Ev "^(\.\.?|\.git)$"`
+dotfiles=`ls -a "${dotfiles_dir}/" | tr ' ' '\n' | grep -E "^\..*$" | grep -Ev "^(\.\.?|\.git)$"`
 echo -e "Following file will be installed:\n${dotfiles[@]}"
 for target in ${dotfiles[@]}; do
   link_file ${target}
