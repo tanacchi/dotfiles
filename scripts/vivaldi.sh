152051f3ec432275c6f974e00962821f5dedd01d
tanacchi <q111026d@mail.kyutech.jp>
2020-04-04 18:51:29 +0900

    Add vivaldi installer

---
+++ scripts/vivaldi.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+wget -qO- https://repo.vivaldi.com/archive/linux_signing_key.pub | sudo apt-key add -
+sudo add-apt-repository 'deb https://repo.vivaldi.com/archive/deb/ stable main'
+sudo apt update && sudo apt install vivaldi-stable
