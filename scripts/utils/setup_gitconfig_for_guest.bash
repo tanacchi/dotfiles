6e21286ee589248fa211afbf1e38edfeb8b676ad
tanacchi <q111026d@mail.kyutech.jp>
2020-04-06 17:13:27 +0900

    Fix guest .gitconfig setupper

--- scripts/utils/setup_gitconfig_for_guest.bash
+++ scripts/utils/setup_gitconfig_for_guest.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-dotfiles_dir="$(cd "$(dirname ${0})"; pwd)/../"
+dotfiles_dir="$(cd "$(dirname ${0})"; pwd)/../.."
 source ${dotfiles_dir}/scripts/utils/functions.bash
 
-curl https://raw.githubusercontent.com/tanacchi/dotfiles/master/.gitconfig > ${HOME}/.gitconfig
+link_file ".gitconfig"
 
 create_git_user_config
