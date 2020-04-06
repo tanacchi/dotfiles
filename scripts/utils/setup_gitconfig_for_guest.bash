cd067cd937820eef5ad3cddd77e8d75dfa3adb46
tanacchi <q111026d@mail.kyutech.jp>
2020-04-06 16:31:55 +0900

    Fix path to functions.bash

--- scripts/utils/setup_gitconfig_for_guest.bash
+++ scripts/utils/setup_gitconfig_for_guest.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 dotfiles_dir="$(cd "$(dirname ${0})"; pwd)/../"
-source ${dotfiles_dir}/scripts/functions.bash
+source ${dotfiles_dir}/scripts/utils/functions.bash
 
 curl https://raw.githubusercontent.com/tanacchi/dotfiles/master/.gitconfig > ${HOME}/.gitconfig
 
