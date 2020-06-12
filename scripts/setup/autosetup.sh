d0c86b28b447d340817cad5318796ff527c4af7e
tanacchi <q111026d@mail.kyutech.jp>
2020-06-13 02:46:09 +0900

    Remove ssh key generation process

--- scripts/setup/autosetup.sh
+++ scripts/setup/autosetup.sh
@@ -22,17 +22,17 @@ git clone https://github.com/tanacchi/dotfiles.git ${dotfiles_dir}
 
 # Generate ssh keys for GitHub
 mkdir "${HOME}/.ssh"
-ssh-keygen -t rsa -b 4096 -f "${HOME}/.ssh/github"
 sh ${scripts_dir}/config/git_ssh_config.sh
-
-# Re-Clone dotfiles repository
-rm -rf ${dotfiles_dir}
-git clone git@github.com:tanacchi/dotfiles.git ${dotfiles_dir}
-
-# Install dotfiles
-bash ${scripts_dir}/setup/install.bash
 
 # Install (or Build) git, vivaldi, vim
 sh   ${scripts_dir}/tools/git.sh
 sh   ${scripts_dir}/tools/vivaldi.sh
 bash ${scripts_dir}/tools/vim.bash
+
+rm -rf ${dotfiles_dir}
+
+# Ending message
+echo "Please make ssh key for github and access the link below to set it up."
+echo "https://github.com/settings/ssh/new\n"
+echo "After that, clone dotfiles with link below."
+echo "git@github.com:tanacchi/dotfiles.git"
