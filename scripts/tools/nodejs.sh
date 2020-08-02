2096747db40bd5733af1d953d50aa0daab7562bb
tanacchi <q111026d@mail.kyutech.jp>
2020-08-03 08:52:35 +0900

    Upgrade nodejs installer

--- scripts/tools/nodejs.sh
+++ scripts/tools/nodejs.sh
@@ -9,4 +9,12 @@ echo "Installing Nodejs (version $1)"
 sudo apt install curl software-properties-common
 echo "https://deb.nodesource.com/setup_${VERSION}.x"
 curl -sL "https://deb.nodesource.com/setup_${VERSION}.x" | sudo bash -
-sudo apt install nodejs
+sudo apt install nodejs npm -y
+
+sudo npm install n -g
+sudo n stable
+
+sudo apt remove --purge -y nodejs
+exec $SHELL
+
+node -v
