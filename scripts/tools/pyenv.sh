08d0347b753f617ade5f763b6b40dde7a0cd2538
tanacchi <q111026d@mail.kyutech.jp>
2021-02-14 15:15:35 +0900

    Modify pyenv.sh to export envs

--- scripts/tools/pyenv.sh
+++ scripts/tools/pyenv.sh
@@ -19,4 +19,7 @@ sudo apt-get install -y build-essential \
                         git
 
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
 pyenv -v
