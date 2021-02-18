b6cbf3c48fee7249035b24504c81cdf3b9000c6d
tanacchi <q111026d@mail.kyutech.jp>
2021-02-19 01:53:48 +0900

    Adjust pyenv installer for MacOS

--- scripts/tools/pyenv.sh
+++ scripts/tools/pyenv.sh
@@ -1,22 +1,24 @@
 #!/bin/sh
 
-sudo apt-get install -y build-essential \
-                        libssl-dev \
-                        zlib1g-dev \
-                        libbz2-dev \
-                        libreadline-dev \
-                        libsqlite3-dev \
-                        wget \
-                        curl \
-                        llvm \
-                        libncurses5-dev \
-                        libncursesw5-dev \
-                        xz-utils \
-                        tk-dev \
-                        libffi-dev \
-                        liblzma-dev \
-                        python-openssl \
-                        git
+PKG_COMMAND="${PKG_COMMAND:-apt -y}"
+sudo $PKG_COMMAND install \
+     build-essential \
+     libssl-dev \
+     zlib1g-dev \
+     libbz2-dev \
+     libreadline-dev \
+     libsqlite3-dev \
+     wget \
+     curl \
+     llvm \
+     libncurses5-dev \
+     libncursesw5-dev \
+     xz-utils \
+     tk-dev \
+     libffi-dev \
+     liblzma-dev \
+     python-openssl \
+     git
 
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 echo "pyenv cloned"
